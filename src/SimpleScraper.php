9b1950c698592c2cc7fdb5296505d6bcb243646e
Carwyn Stephen <caztephen@gmail.com>
2019-03-17 14:55:14 +0100

    Remove comments splitting public/private functions
    Do data retrieval in getData
    
    Split logic that was in constructor into class methods
    
    Remove unused class properties

--- src/SimpleScraper.php
+++ src/SimpleScraper.php
@@ -14,48 +14,65 @@
 */
 namespace Ramonztro\SimpleScraper;
 
-use \DOMDocument;
 use \Exception;
+use \DOMDocument;
+use GuzzleHttp\Client;
 use \InvalidArgumentException;
 
-class SimpleScraper {
-	
-	private
-		$contentType,
-		$data,
-		$content,
-		$httpCode,
-		$url;
-/*===========================================================================*/
-// CONSTRUCTOR
-/*===========================================================================*/
+class SimpleScraper
+{	
+	private $contentType;
+	private $httpClient;
+
 	/**
 	 * 
 	 * @param string $url
 	 * @throws Exception
 	 */
-	public function __construct($url, $userAgent ='') {
-		$this->data = array(
-			'ogp' => array(),
-			'twitter' => array(),
-			'meta' => array()
-		);
-		
+	public function __construct(
+	) {
+		libxml_use_internal_errors(true);
+		$this->httpClient = new Client();
+		$this->userAgent = 'Mozilla/5.0 (compatible; SimpleScraper)';
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function validateURL($url)
+	{
 		$urlPattern = '~^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$~iu';
 		if (!is_string($url))
 			throw new InvalidArgumentException("Argument 'url' is invalid (not a string).");
 		if (!(preg_match($urlPattern, $url)))
 			throw new InvalidArgumentException("Argument 'url' is invalid.");
-		$this->url = $url;
-		if (empty($userAgent))
-			$this->userAgent = 'Mozilla/5.0 (compatible; SimpleScraper)';
-		else
-			$this->userAgent = $userAgent;
+	}
 
-		$this->fetchResource();
-		libxml_use_internal_errors(true);
+	/**
+	 * @return DomDocument
+	 */
+	public function loadDOM($documentBody)
+	{
 		$dom = new DOMDocument(null, 'UTF-8');
-		$dom->loadHTML($this->content);
+		$dom->loadHTML($documentBody);
+
+		return $dom;
+	}
+
+	public function parseTitle($dom)
+	{
+		$titleTags = $dom->getElementsByTagName('title');
+		
+		if($titleTags->length > 0){
+			return $titleTags->item(0)->nodeValue;
+		}
+
+		return null;
+	}
+
+	public function parseMeta($dom)
+	{
+		$data = [];
 		$metaTags = $dom->getElementsByTagName('meta');
 
 		for ($i=0; $i<$metaTags->length; $i++) {
@@ -67,109 +84,54 @@ class SimpleScraper {
 				array_key_exists('property', $attrArray) && 
 				preg_match('~og:([a-zA-Z:_]+)~', $attrArray['property'], $matches)
 			) {
-				$this->data['ogp'][$matches[1]] = $attrArray['content'];
+				$data['og'][$matches[1]] = $attrArray['content'];
 			} else if (
 				array_key_exists('name', $attrArray) &&
 				preg_match('~twitter:([a-zA-Z:_]+)~', $attrArray['name'], $matches)
 			) {
-				$this->data['twitter'][$matches[1]] = $attrArray['content'];
+				$data['twitter'][$matches[1]] = $attrArray['content'];
 			} else if (
 				array_key_exists('name', $attrArray) &&
 				array_key_exists('content', $attrArray)
 			) {
-				$this->data['meta'][$attrArray['name']] = $attrArray['content'];
+				$data['meta'][$attrArray['name']] = $attrArray['content'];
 			}
 		}
-	}
-	
-/*===========================================================================*/
-// PUBLIC METHODS
-/*===========================================================================*/
-	/**
-	 *
-	 * @return array
-	 */
-	public function getAllData() {
-		return $this->data;
-	}
-	
-	/**
-	 *
-	 * @return string
-	 */
-	public function getContent() {
-		return $this->content;
-	}
-	
-	/**
-	 * 
-	 * @return string
-	 */
-	public function getContentType() {
-		return $this->contentType;
+
+		return $data;
 	}
 
 	/**
-	 *
-	 * @return string
-	 */
-	public function getHttpCode() {
-		return $this->httpCode;
-	}
-	
-	/**
-	 *
 	 * @return array
 	 */
-	public function getMeta() {
-		return $this->data['meta'];
+	public function getData($url)
+	{
+		$this->validateURL($url);
+
+		$documentBody = $this->fetchResource($url);
+		$dom = $this->loadDOM($documentBody);
+
+		$data = $this->parseMeta($dom);
+		$title = $this->parseTitle($dom);
+
+		$data['title'] = $title;
+
+		return $data;
+	}
+
+	public function setClient($client)
+	{
+		$this->httpClient = $client;
 	}
 	
-	/**
-	 *
-	 * @return array
-	 */
-	public function getOgp() {
-		return $this->data['ogp'];
-	}
-	
-	/**
-	 *
-	 * @return array
-	 */
-	public function getTwitter() {
-		return $this->data['twitter'];
-	}
-	
-/*===========================================================================*/
-// PRIVATE METHODS
-/*===========================================================================*/
-	private function fetchResource() {
-		$headers = [
-			'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-			'Accept-Encoding: gzip, deflate',
-			'Accept-Language: en-US,en;q=0.5',
-		];
+	private function fetchResource($url) 
+	{
+		$res = $this->httpClient->request('GET', $url);
 		
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_USERAGENT, $this->userAgent);
-		curl_setopt($ch, CURLOPT_URL, $this->url);
-		curl_setopt($ch, CURLOPT_TCP_NODELAY, true);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		$this->content = curl_exec($ch);
-		$info = curl_getinfo($ch);
-		curl_close($ch);
-		
-		$this->httpCode = $info['http_code'];
-		$this->contentType = $info['content_type'];
-		
-		if (((int) $this->httpCode) >= 400) {
-			throw new Exception('STATUS CODE: ' . $this->httpCode);
+		if (((int) $res->getStatusCode()) >= 400) {
+			throw new Exception('STATUS CODE: ' . $res->getStatusCode());
 		}
+
+		return $res->getBody();
 	}
 }
